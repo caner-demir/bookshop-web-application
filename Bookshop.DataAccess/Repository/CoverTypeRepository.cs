1169f3b9007ceb20baa67acd34c0b3d883883113
Caner Demir <cnerdmir@gmail.com>
2021-03-17 03:54:12 +0300

    Implement covertype model
    Implemented covertype model.
    Added covertype entity to repository.

---
+++ Bookshop.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Bookshop.DataAccess.Data;
+using Bookshop.DataAccess.Repository.IRepository;
+using Bookshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bookshop.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var ObjFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+
+            if (ObjFromDb != null)
+            {
+                ObjFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
---
+++ Bookshop.Models/CoverType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bookshop.Models
+{
+    public class CoverType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Cover Type")]
+        public string Name { get; set; }
+    }
+}
